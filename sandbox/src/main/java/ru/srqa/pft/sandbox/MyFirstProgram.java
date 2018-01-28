4624668369aa62cb9764c684167bafa6aa7e275f
And93 <andreidei4ik@mail.ru>
2018-01-28 20:07:22 +0300

    Added several functions

--- sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,29 @@ package ru.srqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] arg) {
+	    hello("Andrei");
 
-		System.out.println("Hello, world");
+	    double l = 5.6;
+
+        System.out.println("The area of a square with a side: " + l + " is: " + area(l) + "\n");
+
+        double a = 54;
+        double b = 11;
+
+        System.out.println("The area of a rectangle with a side a: " + a +
+                ", and b: " + b + " is: " + area(a,b) + "\n");
 	}
+
+	public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!\n");
+    }
+
+    public static double area(double l) {
+	    return l * l;
+    }
+
+    public static double area(double a, double b) {
+	    return a * b;
+    }
 
 }
